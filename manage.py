c5f66f0de819e1d7b1ad4e4db62cee166aa291bb
linjiang <595266161@qq.com>
2018-11-22 17:53:59 +0800

    增加图片展示半成品

--- manage.py
+++ manage.py
@@ -1,22 +1,21 @@
-from flask import request, Flask
+from flask import request, Flask, jsonify
 import time
+import json
 import pymysql
 import os
 app = Flask(__name__)
 
-@app.route("/pictureUpload", methods=['POST'])
+@app.route("/shareUpload", methods=['POST'])
 def pictureUpload():
     upload_file = request.files['file']
     old_file_name = upload_file.filename
     if upload_file:
-        file_path = os.path.join('./src/pics', old_file_name)
+        file_path = os.path.join('./src/img', old_file_name)
         upload_file.save(file_path)
         db = pymysql.connect("localhost","root","123456","iotplatform")
         cursor = db.cursor()
-        a = 'dsf'
-        sql = "insert into picture(location) values(%s)",a
         try:
-            cursor.execute("insert into picture(location) values(%s)",file_path[:30])
+            cursor.execute("insert into share values(null, %s, 1, '2016-1-1 1:1:1')",file_path[:30])
             db.commit()
         except:
             db.rollback()
@@ -26,6 +25,19 @@ def pictureUpload():
         return 'success'
     else:
         return 'failed'
+
+@app.route("/shareShow", methods=['GET'])
+def shareShow():
+    db = pymysql.connect("localhost", "root", "123456", "iotplatform")
+    cursor = db.cursor()
+    try:
+        cursor.execute("select user_ID, location, time from share order by ID desc")
+        temp = cursor.fetchall()
+        db.commit()
+    except:
+        db.rollback()
+    db.close()
+
 
 
 if __name__ == "__main__":
